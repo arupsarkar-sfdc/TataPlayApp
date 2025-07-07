ae26b2a76b3b5f1c8c9cab80bead99177e29785c
Arup Sarkar <arup.sarkar@salesforce.com>
2025-07-07 14:13:25 -0400

    Initial Commit

---
+++ TataPlayApp/TataPlayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TataPlayAppApp.swift
+//  TataPlayApp
+//
+//  Created by Arup Sarkar (TA) on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TataPlayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TataPlayAppTests/TataPlayAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TataPlayAppTests.swift
+//  TataPlayAppTests
+//
+//  Created by Arup Sarkar (TA) on 7/7/25.
+//
+
+import Testing
+
+struct TataPlayAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
